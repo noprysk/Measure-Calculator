9c865a48797fe3dd197543c0cbade9306a4f41e9
noprysk <noprysk@n-ix.com>
2017-10-14 23:41:24 +0300

    Design fixes

--- src/App/components/App.js
+++ src/App/components/App.js
@@ -3,18 +3,27 @@ import { Collapse } from 'antd';
 
 import './App.css';
 import Temperature from './../../Temperature/components/Temperature';
-import 'antd/dist/antd.css';
 
 const Panel = Collapse.Panel;
 
 class App extends Component {
   render() {
     return (
-        <Collapse defaultActiveKey={['1']}>
-            <Panel header="Temperature conversion" key="1">
-                <Temperature />
-            </Panel>
-        </Collapse>
+        <div className='container'>
+            <div className="page-header">
+                <h3>Measure Calculator</h3>
+            </div>
+
+            <div className="row">
+                <div className="col-sm-12">
+                    <Collapse defaultActiveKey={['1']}>
+                        <Panel header="Temperature conversion" key="1">
+                            <Temperature />
+                        </Panel>
+                    </Collapse>
+                </div>
+            </div>
+        </div>
     );
   }
 }
